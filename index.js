b95107ff13ac7e9bfa4d50d6f147a0e740ffecb0
Kaustav2004 <112540025+Kaustav2004@users.noreply.github.com>
2024-03-27 10:17:12 +0530

    Update index.js

--- index.js
+++ index.js
@@ -243,7 +243,7 @@ function autoPlay(){
 
 // main function
 async function main(){
- await getFolder("songs");
+ await getFolder("Media-Player/songs");
  await addFolder();
  await getSogs(`${currFolder}`);
  addsong();
